cfcd6a0d25cca416cb65527c97b22401d38a3d5e
chrizerv <chr_zervas@hotmail.com>
2020-06-11 19:48:02 +0300

    villa data registration completed

--- con_user_villa.php
+++ con_user_villa.php
@@ -12,9 +12,15 @@ if (isset( $_POST['title'], $_POST['prefecture'], $_POST['address'], $_POST['pho
 	$latitude = $_POST['latitude'];
 	$longitude = $_POST['longitude'];
 	$stars = $_POST['stars'];
+	$equipment = "";
+	$username = "karamitros23";
+
+	if (isset($_POST['equipment'])){
+		$equipment =  $_POST['equipment'];
+	}
 
 
-	// --------- ΈΛεγχος με regex. Άν κάτι δέν είανι όπως το θέλουμε τερματίζουμε χωρίς εξήγηση, δεδομένου ότι έχουμε client side validation 
+	// --------- ΈΛεγχος με regex. Άν κάτι δέν είναι όπως το θέλουμε τερματίζουμε χωρίς εξήγηση, δεδομένου ότι έχουμε client side validation. 
 
     //Aναγνώριση και ελληνικών χαρακτήρων.
 	if ( preg_match('/^[a-zA-Z\p{Greek}0-9\s]{4,255}$/u', $title) !== 1 )
@@ -40,12 +46,68 @@ if (isset( $_POST['title'], $_POST['prefecture'], $_POST['address'], $_POST['pho
 
 	if ( preg_match('/^[1-3]{1}$/', $stars) !== 1 )
 		exit(-1);
+
+	// Εφόσον γνωρίζουμε ότι ο εξοπλισμός είναι συνολικά 4 επιλογές , δέν υφίσταται να μας στείλουν 5. 
+	if (count($equipment) > 4)
+		exit(-1);
+
+	// Εφόσον η μορφή των επιλογών είναι προκαθορισμένη, οτίδήποτε περίεργο τρώει πόρτα :D.
+	for($i=0;$i<count($equipment);$i++)
+		if ( preg_match('/^[a-z]{2,11}$/', $equipment[$i]) !== 1 )
+				exit(-1);
+
 	// ------------------------------------------------------------------------
 
 	print_r($_POST);
-	echo 'something';
+	
+	// Προετιμάζουμε τις επιλογές για να τις εισάγουμε στην μορφή του 'SET' data type της db.
+	$equipment = implode(",", $equipment);
+
+	require('db_params.php');
+  try {
+
+    //αρχικοποίηση pdo
+
+  	 $pdoObject = new PDO("mysql:host=$dbhost; dbname=$dbname;", $dbuser, $dbpass);
+     $pdoObject -> exec('set names utf8');
+
+     /*  Exception mode is also useful because you can structure your error handling more clearly than with traditional PHP-style warnings, and with less code/nesting than by running in silent mode and explicitly checking the return value of each database call. 
+     */
+     //Για μεγαλύτερη σιγουριά βάζουμε να 'πετάει' exception σε οτιδήποτε πήγε στραβά.
+     $pdoObject -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+     //εισαγωγή δεδομένων με prepare
+     $sql='INSERT INTO villa (title, prefecture, address, phone, individuals, latitude, longitude, stars, equipment, user_username)
+            VALUES (:title, :prefecture, :address, :phone, :individuals, :latitude, :longitude, :stars, :equipment, :username)';
+
+      $statement = $pdoObject->prepare($sql);
+      
+      $result= $statement->execute( array(       ':title'=>$title,
+                                                 ':prefecture'=>$prefecture,
+                                                 ':address'=>$address,
+                                                 ':phone'=>$phone,
+                                                 ':individuals'=>$individuals,
+                                                 ':latitude'=>$latitude,
+                                                 ':longitude'=>$longitude,
+                                                 ':stars'=>$stars,
+                                                 ':equipment'=> $equipment,
+                                                 ':username'=>$username
+
+                                             ) );
 
 
+      //τερματισμός pdo
+     $statement->closeCursor();
+     $pdoObject = null;
+
+      } catch (PDOException $e) {
+
+        $result=false;
+      	//	echo $e->getMessage();
+      }
+
+      if (!$result)
+      	echo 'Κάτι πήγε στραβά. Προσπαθήστε ξανά!';
 
 }
 
